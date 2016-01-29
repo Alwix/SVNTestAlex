2b5c856bb79cb177024191747a5cc67d14656e0f
Alexander <alexander.bertilsson98@gmail.com>
2016-01-29 16:32:31 +0100

    First iteration of projectile code

---
+++ Assets/ProjectileMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileMove : MonoBehaviour {
+    public int speed;
+    //public Object thisProjectile;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        // Använd transform istället för velocity!!!
+        //GetComponent<Transform>().position = new Vector2(transform.position.x, transform.position.y * speed);
+        
+        
+        //GetComponent<Rigidbody2D>().velocity = new Vector2(transform.position.x, transform.position.y * speed);
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * speed);
+
+
+    }
+}
