634d1fecdf28428b6fae258ba3238af278eb5729
Alexander <alexander.bertilsson98@gmail.com>
2016-02-02 11:29:29 +0100

    Projectile arc

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -1,30 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Projectile : MonoBehaviour {
+public class Projectile : MonoBehaviour
+{
 
-    public int speed;
+    //public int speed;
     public Object projectile;
+
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    void Start()
     {
 
-    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
         if (Input.GetButtonDown("Fire1"))
         {
             //Old
-            //Rigidbody clone;
-            //clone = Instantiate(projectile, new Vector2(transform.position.x, transform.position.y + 0.3f), transform.rotation) as Rigidbody;
+            Rigidbody clone;
+            if (GameObject.FindWithTag("Apple") == null)
+            {
+                clone = Instantiate(projectile, new Vector2(transform.position.x, transform.position.y + 0.3f), transform.rotation) as Rigidbody;
+            }
         }
-      if (Input.GetMouseButton(0))
-        {
 
-        }
+
+
 
     }
 }
